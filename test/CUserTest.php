018d26fbd784c8763d21dcbeb14e09fc7b4d99af
ohavsvik <ohavsvik@gmail.com>
2015-12-14 14:39:27 +0100

    Add unittest

---
+++ test/CUserTest.php
@@ -0,0 +1,31 @@
+<?php
+
+class CUserTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIsAuthenticatedTest()
+    {
+        $user = new CUser(null);
+        $res = $user->isAuthenticated();
+
+        $this->assertFalse($res, "User should not be authenticated");
+
+
+        $_SESSION['user'] = 'ye';
+        $res = $user->isAuthenticated();
+        $this->assertTrue($res, "User should be authenticated");
+    }
+
+    public function testGetName()
+    {
+        $user = new CUser(null);
+        $res = $user->getName();
+        $this->assertEquals(null, $res, "Should not get name.");
+    }
+
+    public function testGetAcronym()
+    {
+        $user = new CUser(null);
+        $res = $user->getAcronym();
+        $this->assertEquals(null, $res, "Should not get acronym.");
+    }
+}
